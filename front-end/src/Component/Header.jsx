47d9c41fe0f24fe5bc143c1a15334ccbed37188e
ghifaripamili <ghifaripamili@gmail.com>
2024-07-31 13:21:35 +0000

    commit2

--- front-end/src/Component/Header.jsx
+++ front-end/src/Component/Header.jsx
@@ -12,15 +12,15 @@ const Header = () => {
   };
 
   return (
-    <div className="flex items-center justify-between fixed text-white px-[20px] top-0 h-[85px] w-full bg-black z-50">
-        <div>
-            <img src="/" alt="logo" />
+    <div className="font-sans flex items-center justify-between fixed text-white px-[20px] top-0 h-[85px] w-full bg-black z-50">
+        <div className="flex items-center gap-[20px]">
+            <img src="/src/assets/logo-shadow-green.png" sizes="100px" className="w-[40px] h-[50px]" alt="logo" />
         </div>
-        <nav className="">
-            <button className="px-[20px]" onClick={() => handleNavigation('home')}>home</button>
-            <button className="px-[20px]" onClick={() => handleNavigation('about')}>About Us</button>
-            <button className="px-[20px] trans" onClick={() => handleNavigation('product')}>Product</button>
-            <button className="px-[20px]" onClick={() => handleNavigation('home')}>Login</button>
+        <nav className="text-[14px] flex flex-row gap-[20px]">
+            <button className="transition ease-in-out duration-300 px-[20px] hover:-translate-y-2 hover:border-b-[2px] tracking-wider hover:border-white" onClick={() => handleNavigation('home')}>home</button>
+            <button className="transition ease-in-out duration-300 px-[20px] hover:-translate-y-2 hover:border-b-[2px] tracking-wider hover:border-white " onClick={() => handleNavigation('about')}>About Us</button>
+            <button className=" transition ease-in-out duration-300 px-[20px] hover:-translate-y-2 hover:border-b-[2px] tracking-wider hover:border-white" onClick={() => handleNavigation('product')}>Product</button>
+            <button className="py-[6px] px-[30px] rounded-full outline outline-[2px] text-white hover:outline-none outline-white hover:font-semibold tracking-wider  hover:bg-white hover:text-blue-500" onClick={() => handleNavigation('home')}>Login</button>
         </nav>
     </div>
   )
